30fdaa7d623aa0abc6ba5d88b0dcdb56b41c72ec
Otito Amuga <urphasin@gmail.com>
2026-04-27 06:36:23 -0600

    dfdf

--- data_structures/arrays.c
+++ data_structures/arrays.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
 
-    int arr[6];
+    srand(time(0));
     
+    int m = 7;
+    int n = 20;
+    int arr[m][n];
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            arr[i][j] = rand() % 2;
+        }
+    }
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
