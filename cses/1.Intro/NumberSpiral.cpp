648641148b1ee3e0fd68ba42c6ac7004f1f8aded
Otito Amuga <urphasin@gmail.com>
2026-05-03 17:40:15 -0600

    a message to the folks: be in maybach

---
+++ cses/1.Intro/NumberSpiral.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int solve(int y, int x) {
+
+}
+
+int main() {
+    int t; cin >> t;
+    
+    while(t--) {
+        int y, x; cin >> y >> x;
+
+        solve(y, x);
+    }
+}
