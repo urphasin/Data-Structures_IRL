35c120f7c187f7741ae68ac6c947f0aa72b01698
Otito Amuga <urphasin@gmail.com>
2026-05-04 10:40:51 -0600

    tiktok keisha

--- cses/1.Intro/NumberSpiral.cpp
+++ cses/1.Intro/NumberSpiral.cpp
@@ -9,7 +9,7 @@ int solve(int y, int x) {
 }
 
 int main() {
-    printf("%f", (1/2 + sqrt(5)/2));
+    printf("Golden ratio: \x1b[95m%f\n\n\x1b[0m", (1/2 + sqrt(5)/2));
     int t; cin >> t;
     
     while(t--) {
